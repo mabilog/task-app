4ec0f4fe829fe185a031ad8765fa992ed1991177
angelo-mab <60443712+angelo-mab@users.noreply.github.com>
2021-05-14 15:43:16 -0400

    changing a few things to make it look better

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Overview from './components/Overview';
+import React, { Component } from 'react';
+import Form from './components/Overview';
 
-function App() {
-  return (
-    <div className="App">
-      <Overview />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      task: {
+        text: '',
+      }
+    }
+  }
+  render() {
+    const { task, tasks } = this.state;
+    return (
+      <div className="App" >
+        <Form />
+      </div>
+    );
+  }
 }
 
 export default App;
